77443888846e79a1a492447f6439c1622a033830
andersapt <andersapt@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-08-19 09:40:48 -0500

    Enabled validation of exact width/height as well as aspect ratio.
    Rewrote and removed the for loop, since the validator will be running per form field anyway, no need to loop, and it was hardcoded to a form field named 'picture' (which was not the case for me).
    
    git-svn-id: http://svn.symfony-project.com/plugins/sfExtendedFileValidatorPlugin/trunk@10949 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/validator/sfExtendedFileValidator.class.php
+++ lib/validator/sfExtendedFileValidator.class.php
@@ -24,58 +24,86 @@ class sfExtendedFileValidator extends sfFileValidator
     {
       return false;
     }
-    else
+    
+    /*
+     * $imgData[0] retrieve width
+     * $imgData[1] retrieve height
+     */
+    $imgData = @getimagesize($value['tmp_name']);
+    
+    $min_width = $this->getParameter('min_width');
+
+    if ($min_width !== null && $min_width > $imgData[0])
     {
-      $imgs = $this->context->getRequest()->getFileNames();
+      $error = $this->getParameter('min_width_error');
 
-      foreach ($imgs as $img)
-      {
-        $img = $this->context->getRequest()->getFilePath($img);
-
-        /*
-         * $imgData[0] retrieve width
-         * $imgData[1] retrieve height
-         */
-        $imgData = @getimagesize($img['picture']);
-
-        $min_width = $this->getParameter('min_width');
-
-        if ($min_width !== null && $min_width > $imgData[0])
-        {
-          $error = $this->getParameter('min_width_error');
-
-          return false;
-        }
-
-        $max_width = $this->getParameter('max_width');
-        if ($max_width !== null && $max_width < $imgData[0])
-        {
-          $error = $this->getParameter('max_width_error');
-
-          return false;
-        }
-
-        $min_height = $this->getParameter('min_height');
-
-        if ($min_height !== null && $min_height > $imgData[1])
-        {
-          $error = $this->getParameter('min_height_error');
-
-          return false;
-        }
-
-        $max_height = $this->getParameter('max_height');
-
-        if ($max_height !== null && $max_height < $imgData[1])
-        {
-          $error = $this->getParameter('max_height_error');
-
-          return false;
-        }
-      }
-
-      return true;
+      return false;
     }
+
+    $max_width = $this->getParameter('max_width');
+    if ($max_width !== null && $max_width < $imgData[0])
+    {
+      $error = $this->getParameter('max_width_error');
+
+      return false;
+    }
+
+    $min_height = $this->getParameter('min_height');
+
+    if ($min_height !== null && $min_height > $imgData[1])
+    {
+      $error = $this->getParameter('min_height_error');
+
+      return false;
+    }
+
+    $max_height = $this->getParameter('max_height');
+
+    if ($max_height !== null && $max_height < $imgData[1])
+    {
+      $error = $this->getParameter('max_height_error');
+
+      return false;
+    }
+    
+    $exact_width = $this->getParameter('exact_width');
+
+    if ($exact_width !== null && $exact_width != $imgData[0])
+    {
+      $error = $this->getParameter('exact_width_error');
+
+      return false;
+    }
+
+    $exact_height = $this->getParameter('exact_height');
+
+    if ($exact_height !== null && $exact_height != $imgData[1])
+    {
+      $error = $this->getParameter('exact_height_error');
+
+      return false;
+    }
+
+    $aspect = $this->getParameter('aspect');
+
+    if ($aspect !== null && ( is_float($aspect) || is_int($aspect) ) && $aspect != $imgData[0]/$imgData[1])
+    {
+      $error = $this->getParameter('aspect_error');
+
+      return false;
+    }
+    
+     // If we want to resave jpegs to noninterlaced (unfortunately this cannot be checked), resave the image using imageinterlace(0) and jpeg quality 90
+     // Todo: Check if the image is noninterlaced before resaving
+    if ( $imgData[2]==IMAGETYPE_JPEG && $this->getParameter('resave_to_noninterlaced') )
+    {
+      $temp_resource = imagecreatefromjpeg($value['tmp_name']);
+      imageinterlace($temp_resource,0);
+      imagejpeg($temp_resource,$value['tmp_name'],90);
+      imagedestroy($temp_resource);
+    }
+    
+    return true;
   }
 
   public function initialize($context, $parameters = null)
@@ -90,7 +118,14 @@ class sfExtendedFileValidator extends sfFileValidator
     $this->setParameter('min_height_error', 'File has a height too small');
     $this->setParameter('max_height', null);
     $this->setParameter('max_height_error', 'File has a height too big');
-
+    $this->setParameter('exact_width', null);
+    $this->setParameter('exact_width_error', 'File has an incorrect width');
+    $this->setParameter('exact_height', null);
+    $this->setParameter('exact_height_error', 'File has an incorrect height');
+    $this->setParameter('aspect', null);
+    $this->setParameter('aspect_error', 'File has an incorrect aspect ratio');
+    $this->setParameter('resave_to_noninterlaced',false);
+    
     $this->getParameterHolder()->add($parameters);
 
     return true;
